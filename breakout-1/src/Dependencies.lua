0292f70fad15231591c7f4dab29f61d89d114c2e
JeremyLoh <jeremyloh03@gmail.com>
2020-12-31 15:53:02 +0800

    breakout-1: Add files from breakout-0

---
+++ breakout-1/src/Dependencies.lua
@@ -0,0 +1,24 @@
+-- push is a library that will allow us to draw our game at a virtual
+-- resolution, instead of however large our window is; used to provide
+-- a more retro aesthetic
+--
+-- https://github.com/Ulydev/push
+push = require "lib/push"
+
+-- the "Class" library allows us to represent anything in
+-- our game as code, rather than keeping track of many
+-- variables and methods
+--
+-- https://github.com/vrld/hump/blob/master/class.lua
+Class = require "lib/class"
+
+-- Import global constants
+require "src/constants"
+
+-- Utility function, mainly used for splitting sprite sheet (atlas) 
+-- into various Quads of different sizes 
+require "src/Util"
+
+require "src/StateMachine"
+require "src/states/BaseState"
+require "src/states/StartState"
