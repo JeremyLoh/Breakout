bf5431edb2f1a9bcc1157327412c9713dce61512
JeremyLoh <jeremyloh03@gmail.com>
2021-01-02 17:13:04 +0800

    breakout-5: Change game over update function

--- breakout-5/src/states/GameOverState.lua
+++ breakout-5/src/states/GameOverState.lua
@@ -6,13 +6,7 @@ end
 
 function GameOverState:update(dt)
     if love.keyboard.wasPressed("enter") or love.keyboard.wasPressed("return") then
-        gStateMachine:change("serve", {
-            ["paddle"] = Paddle(1),
-            ["bricks"] = LevelMaker.createMap(),
-            ["ball"] = Ball(1),
-            ["lives"] = MAX_LIVES,
-            ["score"] = 0,
-        })
+        gStateMachine:change("start")
     elseif love.keyboard.wasPressed("escape") then
         love.event.quit()
     end
@@ -21,7 +15,7 @@ end
 function GameOverState:render()
     local y = VIRTUAL_HEIGHT / 5
     love.graphics.setFont(gFonts["large"])
-    love.graphics.printf("Game Over", 0, y, VIRTUAL_WIDTH, "center")
+    love.graphics.printf("GAME OVER", 0, y, VIRTUAL_WIDTH, "center")
     love.graphics.printf("Score: " .. tostring(self.score), 0, y + 50, VIRTUAL_WIDTH, "center")
     love.graphics.printf("Press \"Enter\" to restart", 0, y + 100, VIRTUAL_WIDTH, "center")
 end
